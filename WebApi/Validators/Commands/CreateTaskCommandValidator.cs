0093cffdb4fc2fac4fb4e027ffd903ebdaa42b32
Harsh Patel <harsh9@outlook.com>
2020-09-17 12:10:50 +0530

    Fluent Validations added for task
    Fluent Validations added for task

---
+++ WebApi/Validators/Commands/CreateTaskCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Commands;
+using FluentValidation;
+
+namespace WebApi.Validators.Commands
+{
+    public class CreateTaskCommandValidator : AbstractValidator<CreateTaskCommand>
+    {
+        public CreateTaskCommandValidator()
+        {          
+            RuleFor(c => c.Subject)
+                .Must((data, name) => !string.IsNullOrEmpty(data.Subject)).WithMessage("Subject is required.")
+                .MaximumLength(255).WithMessage("Subject cannot exceed 255 characters.");
+        }
+    }
+}
---
+++ WebApi/Validators/Commands/UpdateTaskCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Commands;
+using FluentValidation;
+using System;
+
+namespace WebApi.Validators.Commands
+{
+    public class UpdateTaskCommandValidator : AbstractValidator<UpdateTaskCommand>
+    {
+        public UpdateTaskCommandValidator()
+        {
+            RuleFor(c => c.Id)
+                .NotEmpty().WithMessage("Task id is required.")
+                .Must((data, id) => Guid.TryParse(data.Id.ToString(), out var _)).WithMessage("Valid task id is required.");
+
+            RuleFor(c => c.Subject)
+               .Must((data, name) => !string.IsNullOrEmpty(data.Subject)).WithMessage("Subject is required.")
+               .MaximumLength(255).WithMessage("Subject cannot exceed 255 characters.");
+        }
+    }
+}
